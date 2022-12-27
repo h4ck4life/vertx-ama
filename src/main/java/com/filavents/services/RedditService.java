c97966e682c8091b7510b87c77ed1c16af3ba7a0
Alif <MohdAlif-MA.AbdulAziz@aia.com>
2022-12-27 15:03:26 +0800

    Add dummy response in service

--- src/main/java/com/filavents/services/RedditService.java
+++ src/main/java/com/filavents/services/RedditService.java
@@ -1,9 +1,5 @@
 package com.filavents.services;
 
-import com.filavents.services.impl.RedditServiceImpl;
-import com.google.inject.ImplementedBy;
-
-@ImplementedBy(RedditServiceImpl.class)
 public interface RedditService {
     String getRandom();
 }
